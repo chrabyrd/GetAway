1df36ded448636bbb543a985b0db0e5b162b3831
Christopher <chrabyrd@gmail.com>
2017-01-30 11:02:49 -0800

    Setup npm and webpack, basic file structure

---
+++ components/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = props => (
+  <div className="page">
+    <h1>Hello, world!</h1>
+  </div>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: "./entry.jsx",
+  output: {
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["", ".js", ".jsx" ]
+  }
+};
