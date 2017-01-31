7b9337939fa7596aa00c8a986642e2e68ad90ef4
Maria Belgrader <mbelgrader5@gmail.com>
2017-01-31 15:41:49 -0800

    Add App to android index

--- index.android.js
+++ index.android.js
@@ -17,19 +17,7 @@ import App from './components/App';
 export default class LeaveNow extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <App />
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <App />
     );
   }
 }
