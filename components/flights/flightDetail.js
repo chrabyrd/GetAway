bd0d9cca2afadde6bc8824491099a18de47c3d0c
Christopher <chrabyrd@gmail.com>
2017-02-01 15:02:28 -0800

    Create basic styling for detail page

--- components/flights/flightDetail.js
+++ components/flights/flightDetail.js
@@ -7,10 +7,11 @@ class FlightDetail extends Component {
 
     this.state = {
       minPrice: 381,
-      direct: true,
-      outboundLeg: {},
-      inboundLeg: {},
-      quoteDateTime: {}
+      outboundLeg: "SFO to North Korea",
+      inboundLeg: "North Korea to SFO",
+      quoteDateTime: "12:00",
+      countryName: "Italy",
+      cityName: "Milan"
     };
   }
 
@@ -26,11 +27,20 @@ class FlightDetail extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Show Flight Details</Text>
-          <TouchableHighlight style={styles.button} onPress={ () => this._navigate() }>
-              <Text>Back To Flights</Text>
-          </TouchableHighlight>
+      <View style={{backgroundColor: 'green', margin: 5, padding: 5, flex: 1, justifyContent:'center'}}>
+        <View style={{backgroundColor: 'lightblue',margin: 5, padding: 5, flex: 1}}>
+          <View style={{backgroundColor: 'gold',margin: 5, padding: 5, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
+            <Text style={{backgroundColor: 'yellow', margin: 5, padding: 5}}>{this.state.cityName}, {this.state.countryName}</Text>
+            <Text style={{backgroundColor: 'orange', margin: 5, padding: 5}}>${this.state.minPrice}</Text>
+          </View>
+          <View style={{backgroundColor: 'pink',margin: 5, padding: 5, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
+            <Text style={{backgroundColor: 'turquoise', margin: 5, padding: 5}}>{this.state.outboundLeg}</Text>
+            <Text style={{backgroundColor: 'peru', margin: 5, padding: 5}}>{this.state.inboundLeg}</Text>
+          </View>
+        </View>
+        <TouchableHighlight style={styles.button} onPress={ () => this._navigate() }>
+            <Text>Back To Flight Index</Text>
+        </TouchableHighlight>
       </View>
     );
   }
@@ -44,8 +54,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
   },
   button: {
+    flex: .2,
+    margin: 5,
+    padding: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
     borderWidth: 1,
-    backgroundColor: '#9ad3de'
+    backgroundColor: 'ghostwhite'
   }
 });
 
