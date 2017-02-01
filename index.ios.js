66798f153b045b52c97b6437dae59684ed6775cc
Christopher <chrabyrd@gmail.com>
2017-01-31 18:47:47 -0800

    Minor changes

--- index.ios.js
+++ index.ios.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { AppRegistry, StyleSheet } from 'react-native';
-
 import App from './components/app';
 
 AppRegistry.registerComponent('LeaveNow', () => App);
