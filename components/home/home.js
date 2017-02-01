f682ef9bf0d6476b041e53ba3f1f4ab1fdce7bac
Craig Scott <craigtscottjr@gmail.com>
2017-02-01 09:38:49 -0800

    API requesting confirmed

--- components/home/home.js
+++ components/home/home.js
@@ -1,23 +1,42 @@
 import React, {Component} from 'react';
-import {AppRegistry, StyleSheets, Text, View} from 'react-native';
+import {AppRegistry, StyleSheets,
+        Text, View, TextInput, Button} from 'react-native';
+import DatePicker from 'react-native-datepicker';
 
 import api from '../../utils/api';
+let startDate = new Date().toJSON().slice(0,10);
+let bud = "0";
+
+let destinationPlaceOptions = ["VIE", "BUR", "BOJ", "LCA", "PRG", "CPH", "EPU",
+"JYV", "SVL", "ORY","CDG", "NCE", "SFX", "TXL",
+"ATH", "UAK", "LIN", "CIA", "FCO", "KIV", "TGD",
+"TIV", "AMS", "WAW", "MAD", "ARN", "BMA", "NYO",
+"BRN", "BSL", "LHR"];
+Array.prototype.randomDestination = function () {
+  return this[Math.floor(Math.random() * this.length)];
+};
+let destination = destinationPlaceOptions.randomDestination();
+
 
 class Home extends Component {
   constructor(props){
     super(props);
 
     this.state = {
-      flights: []
+      budget: bud,
+      returnDate: startDate,
     };
+    this.onButtonPress = this.onButtonPress.bind(this);
+
   }
-    // componentWillMount(){
-    //   api.getFlights.then((res) => {
-    //     this.setState({
-    //       flights: res
-    //     });
-    //   });
-    // };
+
+
+  onButtonPress() {
+    console.log(this.state);
+    debugger;
+    api.getFlights(destination, this.state.returnDate);
+  };
+
 
 
   render() {
@@ -25,6 +44,39 @@ class Home extends Component {
     return(
       <View>
         <Text>home</Text>
+        <TextInput
+        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={(budget) => this.setState({budget})}
+        value={this.state.budget}
+      />
+      <DatePicker
+       style={{width: 200}}
+       date={this.state.returnDate}
+       mode="date"
+       placeholder="select date"
+       format="YYYY-MM-DD"
+       minDate={startDate}
+       maxDate="2017-12-31"
+       confirmBtnText="Confirm"
+       cancelBtnText="Cancel"
+       customStyles={{
+         dateIcon: {
+           position: 'absolute',
+           left: 0,
+           top: 4,
+           marginLeft: 0
+         },
+         dateInput: {
+           marginLeft: 36
+         }
+       }}
+       onDateChange={(date) => {this.setState({returnDate: date});}}
+     />
+     <Button
+      onPress={this.onButtonPress}
+      title="Find Flights"
+      color="#841584"
+    />
       </View>
     );
   }
