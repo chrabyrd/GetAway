403bac37d2b618a1dd1607ac4e5e13453bf7340b
Craig Scott <craigtscottjr@gmail.com>
2017-02-02 08:53:06 -0800

    Pick form 42 locations

--- util/api.js
+++ util/api.js
@@ -1,3 +1,5 @@
+
+
 export const getSession = () =>{
   fetch(`http://partners.api.skyscanner.net/apiservices/pricing/v1.0`, {
     method: 'POST',
@@ -26,32 +28,48 @@ export const getSession = () =>{
  });
 };
 
-export const getDetail = (url) =>{
-  fetch('http://partners.api.skyscanner.net/apiservices/browsedates/v1.0/US/USD/en-US/SFO/SAN/2017-02-04/2017-02-07?apiKey=cr875483709897975877477133746240', {
-    method: 'get',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-       'Accept': 'application/json'
-    },
-  //   body: JSON.stringify({
-  //    apiKey: 'cr875483709897975877477133746240',
-  //    country: 'US',
-  //    currency: 'USD',
-  //    locale: 'en-US',
-  //    originplace: 'SFO',
-  //    destinationplace: 'VIE',
-  //    outbounddate: '2017-02-04',
-  //    inbounddate: '2017-02-07'
-  //  })
- })
- .then((detail) => detail.json())
- .then((detailJson) => {
-   console.log(detailJson);
-   return detailJson;
- })
- .catch((error) => {
-   console.log(error);
- });
+export const getDetail = () =>{
+  let budget = 1000;
+  destinations = [];
+  count = 0;
+  while ( count < 2){
+     let destinationPlaceOptions = ["SAN", "LAX", "LAS", "DEN", "SEA", "PDX",
+                                    "AKL", "AMS", "BLR", "CDG", "CKG", "CHP",
+                                    "CUN", "DEL", "DUB", "DUS", "DXB", "FRA",
+                                    "HEL", "LHR", "MAN", "MEX", "MUC", "NAN",
+                                    "NRT", "PEK", "PTY", "PVG", "PVR", "SIN",
+                                    "SJD", "SYD", "TAO", "TLV", "TPE", "TXL",
+                                    "YEG", "YUL", "YVR", "YYZ", "ZRH", "CVG"
+                                    ];
+    // DEST = destinationPlaceOptions[(Math.floor(Math.random() * destinationPlaceOptions.length))];
+    DEST = destinationPlaceOptions[count];
+    console.log(DEST);
+    fetch(`http://partners.api.skyscanner.net/apiservices/browsedates/v1.0/US/USD/en-US/SFO/${DEST}/2017-02-04/2017-02-07?apiKey=cr875483709897975877477133746240`, {
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept': 'application/json'
+      },
+
+    })
+    .then((detail) => detail.json())
+    .then((detailJson) => {
+
+      console.log("detail", detailJson);
+      console.log("quote", detailJson.Quotes );
+      if ( detailJson.Quotes.length > 0 ){
+        destinations.push("hi",detailJson);
+      }
+      console.log(destinations);
+
+      // return detailJson;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+    count += 1;
+  }
+  return destinations;
 };
 
 
